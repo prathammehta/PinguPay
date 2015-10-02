23516b5df083a955178e28e9b917bc62950b59bc
Pratham Mehta <prathammehta@outlook.com>
2015-10-02 19:02:06 +0530

    Files now must work

---
+++ The entire thing/PinguPayTests/PinguPayTests.m
@@ -0,0 +1,40 @@
+//
+//  PinguPayTests.m
+//  PinguPayTests
+//
+//  Created by Pratham Mehta on 02/10/15.
+//  Copyright (c) 2015 PinguPay Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PinguPayTests : XCTestCase
+
+@end
+
+@implementation PinguPayTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
